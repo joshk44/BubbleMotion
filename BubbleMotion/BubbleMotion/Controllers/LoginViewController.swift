9c1db4e650f56de843e5e5fae30af95b0b929a12
Jose Ferreyra <joshk44@gmail.com>
2018-08-17 20:13:23 -0300

    Add login view. Change project structure Add Ipad Compatibility

---
+++ BubbleMotion/BubbleMotion/Controllers/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  BubbleMotion
+//
+//  Created by Jose Ferreyra on 17/08/2018.
+//  Copyright © 2018 Arthur Knopper. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userTextView: UITextField!
+    
+    @IBOutlet weak var passwordTextView: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    
+    @IBAction func newUser(_ sender: UIButton) {
+    }
+    
+    @IBAction func justPlay(_ sender: UIButton) {
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Gameplay")
+        self.present (loginViewController!, animated: true)
+    }
+    
+    
+}
